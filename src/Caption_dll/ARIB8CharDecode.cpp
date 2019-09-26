maki-rxrz/Caption2Ass_PCR

commit a34e493ae1da80a88ffa38363dba210138576d79

Caption.dll: Fix initialization code at line feed processing

src/Caption_dll/ARIB8CharDecode.cpp
@@ -319,6 +319,7 @@ void CARIB8CharDecode::InitCaption(void)
 
     m_wMaxPosX = 0;
     m_wTmpPosX = 0;
+    m_wLfPosX = 0;
 
     switch (m_wSWFMode) {
     case 7:
@@ -537,7 +538,7 @@ BOOL CARIB8CharDecode::C0(const BYTE *pbSrc, DWORD *pdwReadSize)
             m_strDecode += "\r\n";
         else {
             CheckModify();
-            m_wPosX = 0;
+            m_wPosX = m_wLfPosX;
             m_wPosY += (m_wCharH + m_wCharVInterval);
             m_wTmpPosX = m_wClientX;
         }
@@ -620,6 +621,7 @@ BOOL CARIB8CharDecode::C0(const BYTE *pbSrc, DWORD *pdwReadSize)
                 m_wPosY = m_wPosY / 2;
         }
         m_wTmpPosX = m_wClientX + m_wPosX;
+        m_wLfPosX = m_wPosX;
 
         dwReadSize = 3;
         break;
@@ -1356,14 +1358,14 @@ BOOL CARIB8CharDecode::AddToString(const char *cDec, BOOL bGaiji)
                 m_wTmpPosX += m_wTmpCharW;
             }
             CheckModify();
-            m_wPosX = 0;
+            m_wPosX = m_wLfPosX;
             m_wPosY += (m_wCharH + m_wCharVInterval);
             m_wTmpPosX = m_wClientX;
         } else {
             for (WORD i = 1; i <= m_wRPC; i++) {
                 if (m_wTmpPosX > (m_wMaxPosX - m_wTmpCharW)) {
                     CheckModify();
-                    m_wPosX = 0;
+                    m_wPosX = m_wLfPosX;
                     m_wPosY += (m_wCharH + m_wCharVInterval);
                     m_wTmpPosX = m_wClientX;
                 }
@@ -1374,7 +1376,7 @@ BOOL CARIB8CharDecode::AddToString(const char *cDec, BOOL bGaiji)
     } else {
         if (m_wTmpPosX > (m_wMaxPosX - m_wTmpCharW)) {
             CheckModify();
-            m_wPosX = 0;
+            m_wPosX = m_wLfPosX;
             m_wPosY += (m_wCharH + m_wCharVInterval);
             m_wTmpPosX = m_wClientX;
         }
@@ -1676,9 +1678,11 @@ BOOL CARIB8CharDecode::CSI(const BYTE *pbSrc, DWORD *pdwReadSize)
                     wParam = wParam * 10 + (pbSrc[i] & 0x0F);
             }
             m_wTmpPosX = m_wPosX;
+            m_wLfPosX = m_wClientX;
             if (m_bUnicode) {
                 m_wPosX += UNICODE_OFFSET;
                 m_wPosY += UNICODE_OFFSET;
+                m_wLfPosX += UNICODE_OFFSET;
             }
         }
         break;

src/Caption_dll/ARIB8CharDecode.h
@@ -212,6 +212,7 @@ protected:
     //map<int, string> dicCharcode_Char_map;
     WORD m_wMaxPosX;
     WORD m_wTmpPosX;
+    WORD m_wLfPosX;
     // RPC‘Î‰ž
     BOOL m_bRPC;
     WORD m_wRPC;

commit 27988aa9286340a243cb05c5ec6ed5fe80db2e36

Caption.dll: Fix a regression in a34e493ae1da80a88ffa38363dba210138576d79

src/Caption_dll/ARIB8CharDecode.cpp
@@ -621,7 +621,7 @@ BOOL CARIB8CharDecode::C0(const BYTE *pbSrc, DWORD *pdwReadSize)
                 m_wPosY = m_wPosY / 2;
         }
         m_wTmpPosX = m_wClientX + m_wPosX;
-        m_wLfPosX = m_wPosX;
+        m_wLfPosX = 0;
 
         dwReadSize = 3;
         break;