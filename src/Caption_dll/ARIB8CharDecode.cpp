maki-rxrz/Caption2Ass_PCR

commit 2f24c42bafc75848a3d20a52c21ca7632e466fa4

all: Cosmetics
- Adjust the number of characters per line
- Organize the common code
- Coding style
- Add static keyword

src/common/CommRoutine.cpp
@@ -11,87 +11,87 @@
 using namespace std;
 
 static const char HiraTable[][3] = {
-    "Çü","Ç†","Ç°","Ç¢","Ç£","Ç§","Ç•",
-    "Ç¶","Çß","Ç®","Ç©","Ç™","Ç´","Ç¨","Ç≠",
-    "ÇÆ","ÇØ","Ç∞","Ç±","Ç≤","Ç≥","Ç¥","Çµ",
-    "Ç∂","Ç∑","Ç∏","Çπ","Ç∫","Çª","Çº","ÇΩ",
-    "Çæ","Çø","Ç¿","Ç¡","Ç¬","Ç√","Çƒ","Ç≈",
-    "Ç∆","Ç«","Ç»","Ç…","Ç ","ÇÀ","ÇÃ","ÇÕ",
-    "ÇŒ","Çœ","Ç–","Ç—","Ç“","Ç”","Ç‘","Ç’",
-    "Ç÷","Ç◊","Çÿ","ÇŸ","Ç⁄","Ç€","Ç‹","Ç›",
-    "Çﬁ","Çﬂ","Ç‡","Ç·","Ç‚","Ç„","Ç‰","ÇÂ",
-    "ÇÊ","ÇÁ","ÇË","ÇÈ","ÇÍ","ÇÎ","ÇÏ","ÇÌ",
-    "ÇÓ","ÇÔ","Ç","ÇÒ","Å@","Å@","Å@","ÅT",
-    "ÅU","Å[","ÅB","Åu","Åv","ÅA","ÅE",
-    "ÅI","Åh","Åî","Åê","Åì","Åï","Åf",
-    "Åi","Åj","Åñ","Å{","ÅC","Å|","ÅD","Å^",
-    "ÇO","ÇP","ÇQ","ÇR","ÇS","ÇT","ÇU","ÇV",
-    "ÇW","ÇX","ÅF","ÅG","ÅÉ","ÅÅ","ÅÑ","ÅH",
-    "Åó","Ç`","Ça","Çb","Çc","Çd","Çe","Çf",
-    "Çg","Çh","Çi","Çj","Çk","Çl","Çm","Çn",
-    "Ço","Çp","Çq","Çr","Çs","Çt","Çu","Çv",
-    "Çw","Çx","Çy","Åm","Åè","Ån","ÅO","ÅQ",
-    "Åe","ÇÅ","ÇÇ","ÇÉ","ÇÑ","ÇÖ","ÇÜ","Çá",
-    "Çà","Çâ","Çä","Çã","Çå","Çç","Çé","Çè",
-    "Çê","Çë","Çí","Çì","Çî","Çï","Çñ","Çó",
-    "Çò","Çô","Çö","Åo","Åb","Åp","ÅP"
+    "Çü", "Ç†", "Ç°", "Ç¢", "Ç£", "Ç§", "Ç•",
+    "Ç¶", "Çß", "Ç®", "Ç©", "Ç™", "Ç´", "Ç¨", "Ç≠",
+    "ÇÆ", "ÇØ", "Ç∞", "Ç±", "Ç≤", "Ç≥", "Ç¥", "Çµ",
+    "Ç∂", "Ç∑", "Ç∏", "Çπ", "Ç∫", "Çª", "Çº", "ÇΩ",
+    "Çæ", "Çø", "Ç¿", "Ç¡", "Ç¬", "Ç√", "Çƒ", "Ç≈",
+    "Ç∆", "Ç«", "Ç»", "Ç…", "Ç ", "ÇÀ", "ÇÃ", "ÇÕ",
+    "ÇŒ", "Çœ", "Ç–", "Ç—", "Ç“", "Ç”", "Ç‘", "Ç’",
+    "Ç÷", "Ç◊", "Çÿ", "ÇŸ", "Ç⁄", "Ç€", "Ç‹", "Ç›",
+    "Çﬁ", "Çﬂ", "Ç‡", "Ç·", "Ç‚", "Ç„", "Ç‰", "ÇÂ",
+    "ÇÊ", "ÇÁ", "ÇË", "ÇÈ", "ÇÍ", "ÇÎ", "ÇÏ", "ÇÌ",
+    "ÇÓ", "ÇÔ", "Ç", "ÇÒ", "Å@", "Å@", "Å@", "ÅT",
+    "ÅU", "Å[", "ÅB", "Åu", "Åv", "ÅA", "ÅE",
+    "ÅI", "Åh", "Åî", "Åê", "Åì", "Åï", "Åf",
+    "Åi", "Åj", "Åñ", "Å{", "ÅC", "Å|", "ÅD", "Å^",
+    "ÇO", "ÇP", "ÇQ", "ÇR", "ÇS", "ÇT", "ÇU", "ÇV",
+    "ÇW", "ÇX", "ÅF", "ÅG", "ÅÉ", "ÅÅ", "ÅÑ", "ÅH",
+    "Åó", "Ç`", "Ça", "Çb", "Çc", "Çd", "Çe", "Çf",
+    "Çg", "Çh", "Çi", "Çj", "Çk", "Çl", "Çm", "Çn",
+    "Ço", "Çp", "Çq", "Çr", "Çs", "Çt", "Çu", "Çv",
+    "Çw", "Çx", "Çy", "Åm", "Åè", "Ån", "ÅO", "ÅQ",
+    "Åe", "ÇÅ", "ÇÇ", "ÇÉ", "ÇÑ", "ÇÖ", "ÇÜ", "Çá",
+    "Çà", "Çâ", "Çä", "Çã", "Çå", "Çç", "Çé", "Çè",
+    "Çê", "Çë", "Çí", "Çì", "Çî", "Çï", "Çñ", "Çó",
+    "Çò", "Çô", "Çö", "Åo", "Åb", "Åp", "ÅP"
 };
 
 static const char HalfHiraTable[][3] = {
-    "Çü","Ç†","Ç°","Ç¢","Ç£","Ç§","Ç•",
-    "Ç¶","Çß","Ç®","Ç©","Ç™","Ç´","Ç¨","Ç≠",
-    "ÇÆ","ÇØ","Ç∞","Ç±","Ç≤","Ç≥","Ç¥","Çµ",
-    "Ç∂","Ç∑","Ç∏","Çπ","Ç∫","Çª","Çº","ÇΩ",
-    "Çæ","Çø","Ç¿","Ç¡","Ç¬","Ç√","Çƒ","Ç≈",
-    "Ç∆","Ç«","Ç»","Ç…","Ç ","ÇÀ","ÇÃ","ÇÕ",
-    "ÇŒ","Çœ","Ç–","Ç—","Ç“","Ç”","Ç‘","Ç’",
-    "Ç÷","Ç◊","Çÿ","ÇŸ","Ç⁄","Ç€","Ç‹","Ç›",
-    "Çﬁ","Çﬂ","Ç‡","Ç·","Ç‚","Ç„","Ç‰","ÇÂ",
-    "ÇÊ","ÇÁ","ÇË","ÇÈ","ÇÍ","ÇÎ","ÇÏ","ÇÌ",
-    "ÇÓ","ÇÔ","Ç","ÇÒ"," "," "," ","ÅT",
-    "ÅU","∞","°","¢","£","§","•",
-    "!","\"","#","$","%","&","'",
-    "(",")","*","+",",","-",".","/",
-    "0","1","2","3","4","5","6","7",
-    "8","9",":",";","<","=",">","?",
-    "@","A","B","C","D","E","F","G",
-    "H","I","J","K","L","M","N","O",
-    "P","Q","R","S","T","U","V","W",
-    "X","Y","Z","[","Åè","]","^","_",
-    "`","a","b","c","d","e","f","g",
-    "h","i","j","k","l","m","n","o",
-    "p","q","r","s","t","u","v","w",
-    "x","y","z","{","|","}","ÅP"
+    "Çü", "Ç†", "Ç°", "Ç¢", "Ç£", "Ç§", "Ç•",
+    "Ç¶", "Çß", "Ç®", "Ç©", "Ç™", "Ç´", "Ç¨", "Ç≠",
+    "ÇÆ", "ÇØ", "Ç∞", "Ç±", "Ç≤", "Ç≥", "Ç¥", "Çµ",
+    "Ç∂", "Ç∑", "Ç∏", "Çπ", "Ç∫", "Çª", "Çº", "ÇΩ",
+    "Çæ", "Çø", "Ç¿", "Ç¡", "Ç¬", "Ç√", "Çƒ", "Ç≈",
+    "Ç∆", "Ç«", "Ç»", "Ç…", "Ç ", "ÇÀ", "ÇÃ", "ÇÕ",
+    "ÇŒ", "Çœ", "Ç–", "Ç—", "Ç“", "Ç”", "Ç‘", "Ç’",
+    "Ç÷", "Ç◊", "Çÿ", "ÇŸ", "Ç⁄", "Ç€", "Ç‹", "Ç›",
+    "Çﬁ", "Çﬂ", "Ç‡", "Ç·", "Ç‚", "Ç„", "Ç‰", "ÇÂ",
+    "ÇÊ", "ÇÁ", "ÇË", "ÇÈ", "ÇÍ", "ÇÎ", "ÇÏ", "ÇÌ",
+    "ÇÓ", "ÇÔ", "Ç", "ÇÒ", " ", " ", " ", "ÅT",
+    "ÅU", "∞", "°", "¢", "£", "§", "•",
+    "!", "\"", "#", "$", "%", "&", "'",
+    "(", ")", "*", "+", ", ", "-", ".", "/",
+    "0", "1", "2", "3", "4", "5", "6", "7",
+    "8", "9", ":", ";", "<", "=", ">", "?",
+    "@", "A", "B", "C", "D", "E", "F", "G",
+    "H", "I", "J", "K", "L", "M", "N", "O",
+    "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z", "[", "Åè", "]", "^", "_",
+    "`", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z", "{", "|", "}", "ÅP"
 };
 
 static const char KanaTable[][3] = {
-    "É@","ÉA","ÉB","ÉC","ÉD","ÉE","ÉF",
-    "ÉG","ÉH","ÉI","ÉJ","ÉK","ÉL","ÉM","ÉN",
-    "ÉO","ÉP","ÉQ","ÉR","ÉS","ÉT","ÉU","ÉV",
-    "ÉW","ÉX","ÉY","ÉZ","É[","É\","É]","É^",
-    "É_","É`","Éa","Éb","Éc","Éd","Ée","Éf",
-    "Ég","Éh","Éi","Éj","Ék","Él","Ém","Én",
-    "Éo","Ép","Éq","Ér","És","Ét","Éu","Év",
-    "Éw","Éx","Éy","Éz","É{","É|","É}","É~",
-    "ÉÄ","ÉÅ","ÉÇ","ÉÉ","ÉÑ","ÉÖ","ÉÜ","Éá",
-    "Éà","Éâ","Éä","Éã","Éå","Éç","Éé","Éè",
-    "Éê","Éë","Éí","Éì","Éî","Éï","Éñ","ÅR",
-    "ÅS","Å[","ÅB","Åu","Åv","ÅA","ÅE"
+    "É@", "ÉA", "ÉB", "ÉC", "ÉD", "ÉE", "ÉF",
+    "ÉG", "ÉH", "ÉI", "ÉJ", "ÉK", "ÉL", "ÉM", "ÉN",
+    "ÉO", "ÉP", "ÉQ", "ÉR", "ÉS", "ÉT", "ÉU", "ÉV",
+    "ÉW", "ÉX", "ÉY", "ÉZ", "É[", "É\", "É]", "É^",
+    "É_", "É`", "Éa", "Éb", "Éc", "Éd", "Ée", "Éf",
+    "Ég", "Éh", "Éi", "Éj", "Ék", "Él", "Ém", "Én",
+    "Éo", "Ép", "Éq", "Ér", "És", "Ét", "Éu", "Év",
+    "Éw", "Éx", "Éy", "Éz", "É{", "É|", "É}", "É~",
+    "ÉÄ", "ÉÅ", "ÉÇ", "ÉÉ", "ÉÑ", "ÉÖ", "ÉÜ", "Éá",
+    "Éà", "Éâ", "Éä", "Éã", "Éå", "Éç", "Éé", "Éè",
+    "Éê", "Éë", "Éí", "Éì", "Éî", "Éï", "Éñ", "ÅR",
+    "ÅS", "Å[", "ÅB", "Åu", "Åv", "ÅA", "ÅE"
 };
 
 static const char HalfKanaTable[][3] = {
-    "ß","±","®","≤","©","≥","™",
-    "¥","´","µ","∂","∂ﬁ","∑","∑ﬁ","∏",
-    "∏ﬁ","π","πﬁ","∫","∫ﬁ","ª","ªﬁ","º",
-    "ºﬁ","Ω","Ωﬁ","æ","æﬁ","ø","øﬁ","¿",
-    "¿ﬁ","¡","¡ﬁ","Ø","¬","¬ﬁ","√","√ﬁ",
-    "ƒ","ƒﬁ","≈","∆","«","»","…"," ",
-    " ﬁ"," ﬂ","À","Àﬁ","Àﬂ","Ã","Ãﬁ","Ãﬂ",
-    "Õ","Õﬁ","Õﬂ","Œ","Œﬁ","Œﬂ","œ","–",
-    "—","“","”","¨","‘","≠","’","Æ",
-    "÷","◊","ÿ","Ÿ","⁄","€","Éé","‹",
-    "Éê","Éë","¶","›","≥ﬁ","Éï","Éñ","ÅR",
-    "ÅS","∞","°","¢","£","§","•"
+    "ß", "±", "®", "≤", "©", "≥", "™",
+    "¥", "´", "µ", "∂", "∂ﬁ", "∑", "∑ﬁ", "∏",
+    "∏ﬁ", "π", "πﬁ", "∫", "∫ﬁ", "ª", "ªﬁ", "º",
+    "ºﬁ", "Ω", "Ωﬁ", "æ", "æﬁ", "ø", "øﬁ", "¿",
+    "¿ﬁ", "¡", "¡ﬁ", "Ø", "¬", "¬ﬁ", "√", "√ﬁ",
+    "ƒ", "ƒﬁ", "≈", "∆", "«", "»", "…", " ",
+    " ﬁ", " ﬂ", "À", "Àﬁ", "Àﬂ", "Ã", "Ãﬁ", "Ãﬂ",
+    "Õ", "Õﬁ", "Õﬂ", "Œ", "Œﬁ", "Œﬂ", "œ", "–",
+    "—", "“", "”", "¨", "‘", "≠", "’", "Æ",
+    "÷", "◊", "ÿ", "Ÿ", "⁄", "€", "Éé", "‹",
+    "Éê", "Éë", "¶", "›", "≥ﬁ", "Éï", "Éñ", "ÅR",
+    "ÅS", "∞", "°", "¢", "£", "§", "•"
 };
 
 #ifdef _DEBUG

src/Caption_dll/Caption/ARIB8CharDecode.cpp
@@ -14,193 +14,193 @@
 #include "ARIBGaiji.h"
 
 static const CLUT_DAT DefClut[] = {
-    {  0,  0,  0,255}, //0
-    {255,  0,  0,255},
-    {  0,255,  0,255},
-    {255,255,  0,255},
-    {  0,  0,255,255},
-    {255,  0,255,255}, //5
-    {  0,255,255,255},
-    {255,255,255,255},
-    {  0,  0,  0,  0},
-    {170,  0,  0,255},
-    {  0,170,  0,255}, //10
-    {170,170,  0,255},
-    {  0,  0,170,255},
-    {170,  0,170,255},
-    {  0,170,170,255},
-    {170,170,170,255}, //15
-    {  0,  0, 85,255},
-    {  0, 85,  0,255},
-    {  0, 85, 85,255},
-    {  0, 85,170,255},
-    {  0, 85,255,255}, //20
-    {  0,170, 85,255},
-    {  0,170,255,255},
-    {  0,255, 85,255},
-    {  0,255,170,255},
-    { 85,  0,  0,255}, //25
-    { 85,  0, 85,255},
-    { 85,  0,170,255},
-    { 85,  0,255,255},
-    { 85, 85,  0,255},
-    { 85, 85, 85,255}, //30
-    { 85, 85,170,255},
-    { 85, 85,255,255},
-    { 85,170,  0,255},
-    { 85,170, 85,255},
-    { 85,170,170,255}, //35
-    { 85,170,255,255},
-    { 85,255,  0,255},
-    { 85,255, 85,255},
-    { 85,255,170,255},
-    { 85,255,255,255}, //40
-    {170,  0, 85,255},
-    {170,  0,255,255},
-    {170, 85,  0,255},
-    {170, 85, 85,255},
-    {170, 85,170,255}, //45
-    {170, 85,255,255},
-    {170,170, 85,255},
-    {170,170,255,255},
-    {170,255,  0,255},
-    {170,255, 85,255}, //50
-    {170,255,170,255},
-    {170,255,255,255},
-    {255,  0, 85,255},
-    {255,  0,170,255},
-    {255, 85,  0,255}, //55
-    {255, 85, 85,255},
-    {255, 85,170,255},
-    {255, 85,255,255},
-    {255,170,  0,255},
-    {255,170, 85,255}, //60
-    {255,170,170,255},
-    {255,170,255,255},
-    {255,255, 85,255},
-    {255,255,170,255},
-    {  0,  0,  0,128}, //65
-    {255,  0,  0,128},
-    {  0,255,  0,128},
-    {255,255,  0,128},
-    {  0,  0,255,128},
-    {255,  0,255,128}, //70
-    {  0,255,255,128},
-    {255,255,255,128},
-    {170,  0,  0,128},
-    {  0,170,  0,128},
-    {170,170,  0,128}, //75
-    {  0,  0,170,128},
-    {170,  0,170,128},
-    {  0,170,170,128},
-    {170,170,170,128},
-    {  0,  0, 85,128}, //80
-    {  0, 85,  0,128},
-    {  0, 85, 85,128},
-    {  0, 85,170,128},
-    {  0, 85,255,128},
-    {  0,170, 85,128}, //85
-    {  0,170,255,128},
-    {  0,255, 85,128},
-    {  0,255,170,128},
-    { 85,  0,  0,128},
-    { 85,  0, 85,128}, //90
-    { 85,  0,170,128},
-    { 85,  0,255,128},
-    { 85, 85,  0,128},
-    { 85, 85, 85,128},
-    { 85, 85,170,128}, //95
-    { 85, 85,255,128},
-    { 85,170,  0,128},
-    { 85,170, 85,128},
-    { 85,170,170,128},
-    { 85,170,255,128}, //100
-    { 85,255,  0,128},
-    { 85,255, 85,128},
-    { 85,255,170,128},
-    { 85,255,255,128},
-    {170,  0, 85,128}, //105
-    {170,  0,255,128},
-    {170, 85,  0,128},
-    {170, 85, 85,128},
-    {170, 85,170,128},
-    {170, 85,255,128}, //110
-    {170,170, 85,128},
-    {170,170,255,128},
-    {170,255,  0,128},
-    {170,255, 85,128},
-    {170,255,170,128}, //115
-    {170,255,255,128},
-    {255,  0, 85,128},
-    {255,  0,170,128},
-    {255, 85,  0,128},
-    {255, 85, 85,128}, //120
-    {255, 85,170,128},
-    {255, 85,255,128},
-    {255,170,  0,128},
-    {255,170, 85,128},
-    {255,170,170,128}, //125
-    {255,170,255,128},
-    {255,255, 85,128}
+    {   0,   0,   0, 255 }, //0
+    { 255,   0,   0, 255 },
+    {   0, 255,   0, 255 },
+    { 255, 255,   0, 255 },
+    {   0,   0, 255, 255 },
+    { 255,   0, 255, 255 }, //5
+    {   0, 255, 255, 255 },
+    { 255, 255, 255, 255 },
+    {   0,   0,   0,   0 },
+    { 170,   0,   0, 255 },
+    {   0, 170,   0, 255 }, //10
+    { 170, 170,   0, 255 },
+    {   0,   0, 170, 255 },
+    { 170,   0, 170, 255 },
+    {   0, 170, 170, 255 },
+    { 170, 170, 170, 255 }, //15
+    {   0,   0,  85, 255 },
+    {   0,  85,   0, 255 },
+    {   0,  85,  85, 255 },
+    {   0,  85, 170, 255 },
+    {   0,  85, 255, 255 }, //20
+    {   0, 170,  85, 255 },
+    {   0, 170, 255, 255 },
+    {   0, 255,  85, 255 },
+    {   0, 255, 170, 255 },
+    {  85,   0,   0, 255 }, //25
+    {  85,   0,  85, 255 },
+    {  85,   0, 170, 255 },
+    {  85,   0, 255, 255 },
+    {  85,  85,   0, 255 },
+    {  85,  85,  85, 255 }, //30
+    {  85,  85, 170, 255 },
+    {  85,  85, 255, 255 },
+    {  85, 170,   0, 255 },
+    {  85, 170,  85, 255 },
+    {  85, 170, 170, 255 }, //35
+    {  85, 170, 255, 255 },
+    {  85, 255,   0, 255 },
+    {  85, 255,  85, 255 },
+    {  85, 255, 170, 255 },
+    {  85, 255, 255, 255 }, //40
+    { 170,   0,  85, 255 },
+    { 170,   0, 255, 255 },
+    { 170,  85,   0, 255 },
+    { 170,  85,  85, 255 },
+    { 170,  85, 170, 255 }, //45
+    { 170,  85, 255, 255 },
+    { 170, 170,  85, 255 },
+    { 170, 170, 255, 255 },
+    { 170, 255,   0, 255 },
+    { 170, 255,  85, 255 }, //50
+    { 170, 255, 170, 255 },
+    { 170, 255, 255, 255 },
+    { 255,   0,  85, 255 },
+    { 255,   0, 170, 255 },
+    { 255,  85,   0, 255 }, //55
+    { 255,  85,  85, 255 },
+    { 255,  85, 170, 255 },
+    { 255,  85, 255, 255 },
+    { 255, 170,   0, 255 },
+    { 255, 170,  85, 255 }, //60
+    { 255, 170, 170, 255 },
+    { 255, 170, 255, 255 },
+    { 255, 255,  85, 255 },
+    { 255, 255, 170, 255 },
+    {   0,   0,   0, 128 }, //65
+    { 255,   0,   0, 128 },
+    {   0, 255,   0, 128 },
+    { 255, 255,   0, 128 },
+    {   0,   0, 255, 128 },
+    { 255,   0, 255, 128 }, //70
+    {   0, 255, 255, 128 },
+    { 255, 255, 255, 128 },
+    { 170,   0,   0, 128 },
+    {   0, 170,   0, 128 },
+    { 170, 170,   0, 128 }, //75
+    {   0,   0, 170, 128 },
+    { 170,   0, 170, 128 },
+    {   0, 170, 170, 128 },
+    { 170, 170, 170, 128 },
+    {   0,   0,  85, 128 }, //80
+    {   0,  85,   0, 128 },
+    {   0,  85,  85, 128 },
+    {   0,  85, 170, 128 },
+    {   0,  85, 255, 128 },
+    {   0, 170,  85, 128 }, //85
+    {   0, 170, 255, 128 },
+    {   0, 255,  85, 128 },
+    {   0, 255, 170, 128 },
+    {  85,   0,   0, 128 },
+    {  85,   0,  85, 128 }, //90
+    {  85,   0, 170, 128 },
+    {  85,   0, 255, 128 },
+    {  85,  85,   0, 128 },
+    {  85,  85,  85, 128 },
+    {  85,  85, 170, 128 }, //95
+    {  85,  85, 255, 128 },
+    {  85, 170,   0, 128 },
+    {  85, 170,  85, 128 },
+    {  85, 170, 170, 128 },
+    {  85, 170, 255, 128 }, //100
+    {  85, 255,   0, 128 },
+    {  85, 255,  85, 128 },
+    {  85, 255, 170, 128 },
+    {  85, 255, 255, 128 },
+    { 170,   0,  85, 128 }, //105
+    { 170,   0, 255, 128 },
+    { 170,  85,   0, 128 },
+    { 170,  85,  85, 128 },
+    { 170,  85, 170, 128 },
+    { 170,  85, 255, 128 }, //110
+    { 170, 170,  85, 128 },
+    { 170, 170, 255, 128 },
+    { 170, 255,   0, 128 },
+    { 170, 255,  85, 128 },
+    { 170, 255, 170, 128 }, //115
+    { 170, 255, 255, 128 },
+    { 255,   0,  85, 128 },
+    { 255,   0, 170, 128 },
+    { 255,  85,   0, 128 },
+    { 255,  85,  85, 128 }, //120
+    { 255,  85, 170, 128 },
+    { 255,  85, 255, 128 },
+    { 255, 170,   0, 128 },
+    { 255, 170,  85, 128 },
+    { 255, 170, 170, 128 }, //125
+    { 255, 170, 255, 128 },
+    { 255, 255,  85, 128 }
 };
 
 static const char AsciiTable[][3] = {
-    "ÅI","Åh","Åî","Åê","Åì","Åï","Åf",
-    "Åi","Åj","Åñ","Å{","ÅC","Å|","ÅD","Å^",
-    "ÇO","ÇP","ÇQ","ÇR","ÇS","ÇT","ÇU","ÇV",
-    "ÇW","ÇX","ÅF","ÅG","ÅÉ","ÅÅ","ÅÑ","ÅH",
-    "Åó","Ç`","Ça","Çb","Çc","Çd","Çe","Çf",
-    "Çg","Çh","Çi","Çj","Çk","Çl","Çm","Çn",
-    "Ço","Çp","Çq","Çr","Çs","Çt","Çu","Çv",
-    "Çw","Çx","Çy","Åm","Åè","Ån","ÅO","ÅQ",
-    "Åe","ÇÅ","ÇÇ","ÇÉ","ÇÑ","ÇÖ","ÇÜ","Çá",
-    "Çà","Çâ","Çä","Çã","Çå","Çç","Çé","Çè",
-    "Çê","Çë","Çí","Çì","Çî","Çï","Çñ","Çó",
-    "Çò","Çô","Çö","Åo","Åb","Åp","ÅP"
+    "ÅI", "Åh", "Åî", "Åê", "Åì", "Åï", "Åf",
+    "Åi", "Åj", "Åñ", "Å{", "ÅC", "Å|", "ÅD", "Å^",
+    "ÇO", "ÇP", "ÇQ", "ÇR", "ÇS", "ÇT", "ÇU", "ÇV",
+    "ÇW", "ÇX", "ÅF", "ÅG", "ÅÉ", "ÅÅ", "ÅÑ", "ÅH",
+    "Åó", "Ç`", "Ça", "Çb", "Çc", "Çd", "Çe", "Çf",
+    "Çg", "Çh", "Çi", "Çj", "Çk", "Çl", "Çm", "Çn",
+    "Ço", "Çp", "Çq", "Çr", "Çs", "Çt", "Çu", "Çv",
+    "Çw", "Çx", "Çy", "Åm", "Åè", "Ån", "ÅO", "ÅQ",
+    "Åe", "ÇÅ", "ÇÇ", "ÇÉ", "ÇÑ", "ÇÖ", "ÇÜ", "Çá",
+    "Çà", "Çâ", "Çä", "Çã", "Çå", "Çç", "Çé", "Çè",
+    "Çê", "Çë", "Çí", "Çì", "Çî", "Çï", "Çñ", "Çó",
+    "Çò", "Çô", "Çö", "Åo", "Åb", "Åp", "ÅP"
 };
 static const char HiraTable[][3] = {
-    "Çü","Ç†","Ç°","Ç¢","Ç£","Ç§","Ç•",
-    "Ç¶","Çß","Ç®","Ç©","Ç™","Ç´","Ç¨","Ç≠",
-    "ÇÆ","ÇØ","Ç∞","Ç±","Ç≤","Ç≥","Ç¥","Çµ",
-    "Ç∂","Ç∑","Ç∏","Çπ","Ç∫","Çª","Çº","ÇΩ",
-    "Çæ","Çø","Ç¿","Ç¡","Ç¬","Ç√","Çƒ","Ç≈",
-    "Ç∆","Ç«","Ç»","Ç…","Ç ","ÇÀ","ÇÃ","ÇÕ",
-    "ÇŒ","Çœ","Ç–","Ç—","Ç“","Ç”","Ç‘","Ç’",
-    "Ç÷","Ç◊","Çÿ","ÇŸ","Ç⁄","Ç€","Ç‹","Ç›",
-    "Çﬁ","Çﬂ","Ç‡","Ç·","Ç‚","Ç„","Ç‰","ÇÂ",
-    "ÇÊ","ÇÁ","ÇË","ÇÈ","ÇÍ","ÇÎ","ÇÏ","ÇÌ",
-    "ÇÓ","ÇÔ","Ç","ÇÒ","Å@","Å@","Å@","ÅT",
-    "ÅU","Å[","ÅB","Åu","Åv","ÅA","ÅE"
+    "Çü", "Ç†", "Ç°", "Ç¢", "Ç£", "Ç§", "Ç•",
+    "Ç¶", "Çß", "Ç®", "Ç©", "Ç™", "Ç´", "Ç¨", "Ç≠",
+    "ÇÆ", "ÇØ", "Ç∞", "Ç±", "Ç≤", "Ç≥", "Ç¥", "Çµ",
+    "Ç∂", "Ç∑", "Ç∏", "Çπ", "Ç∫", "Çª", "Çº", "ÇΩ",
+    "Çæ", "Çø", "Ç¿", "Ç¡", "Ç¬", "Ç√", "Çƒ", "Ç≈",
+    "Ç∆", "Ç«", "Ç»", "Ç…", "Ç ", "ÇÀ", "ÇÃ", "ÇÕ",
+    "ÇŒ", "Çœ", "Ç–", "Ç—", "Ç“", "Ç”", "Ç‘", "Ç’",
+    "Ç÷", "Ç◊", "Çÿ", "ÇŸ", "Ç⁄", "Ç€", "Ç‹", "Ç›",
+    "Çﬁ", "Çﬂ", "Ç‡", "Ç·", "Ç‚", "Ç„", "Ç‰", "ÇÂ",
+    "ÇÊ", "ÇÁ", "ÇË", "ÇÈ", "ÇÍ", "ÇÎ", "ÇÏ", "ÇÌ",
+    "ÇÓ", "ÇÔ", "Ç", "ÇÒ", "Å@", "Å@", "Å@", "ÅT",
+    "ÅU", "Å[", "ÅB", "Åu", "Åv", "ÅA", "ÅE"
 };
 static const char KanaTable[][3] = {
-    "É@","ÉA","ÉB","ÉC","ÉD","ÉE","ÉF",
-    "ÉG","ÉH","ÉI","ÉJ","ÉK","ÉL","ÉM","ÉN",
-    "ÉO","ÉP","ÉQ","ÉR","ÉS","ÉT","ÉU","ÉV",
-    "ÉW","ÉX","ÉY","ÉZ","É[","É\","É]","É^",
-    "É_","É`","Éa","Éb","Éc","Éd","Ée","Éf",
-    "Ég","Éh","Éi","Éj","Ék","Él","Ém","Én",
-    "Éo","Ép","Éq","Ér","És","Ét","Éu","Év",
-    "Éw","Éx","Éy","Éz","É{","É|","É}","É~",
-    "ÉÄ","ÉÅ","ÉÇ","ÉÉ","ÉÑ","ÉÖ","ÉÜ","Éá",
-    "Éà","Éâ","Éä","Éã","Éå","Éç","Éé","Éè",
-    "Éê","Éë","Éí","Éì","Éî","Éï","Éñ","ÅR",
-    "ÅS","Å[","ÅB","Åu","Åv","ÅA","ÅE"
+    "É@", "ÉA", "ÉB", "ÉC", "ÉD", "ÉE", "ÉF",
+    "ÉG", "ÉH", "ÉI", "ÉJ", "ÉK", "ÉL", "ÉM", "ÉN",
+    "ÉO", "ÉP", "ÉQ", "ÉR", "ÉS", "ÉT", "ÉU", "ÉV",
+    "ÉW", "ÉX", "ÉY", "ÉZ", "É[", "É\", "É]", "É^",
+    "É_", "É`", "Éa", "Éb", "Éc", "Éd", "Ée", "Éf",
+    "Ég", "Éh", "Éi", "Éj", "Ék", "Él", "Ém", "Én",
+    "Éo", "Ép", "Éq", "Ér", "És", "Ét", "Éu", "Év",
+    "Éw", "Éx", "Éy", "Éz", "É{", "É|", "É}", "É~",
+    "ÉÄ", "ÉÅ", "ÉÇ", "ÉÉ", "ÉÑ", "ÉÖ", "ÉÜ", "Éá",
+    "Éà", "Éâ", "Éä", "Éã", "Éå", "Éç", "Éé", "Éè",
+    "Éê", "Éë", "Éí", "Éì", "Éî", "Éï", "Éñ", "ÅR",
+    "ÅS", "Å[", "ÅB", "Åu", "Åv", "ÅA", "ÅE"
 };
 
 // îºäpópÇÃÉeÅ[ÉuÉã
 static const char HalfAsciiTable[][3] = {
-    "!","\"","#","$","%","&","'",
-    "(",")","*","+",",","-",".","/",
-    "0","1","2","3","4","5","6","7",
-    "8","9",":",";","<","=",">","?",
-    "@","A","B","C","D","E","F","G",
-    "H","I","J","K","L","M","N","O",
-    "P","Q","R","S","T","U","V","W",
-    "X","Y","Z","[","\\","]","^","_",
-    "'","a","b","c","d","e","f","g",
-    "h","i","j","k","l","m","n","o",
-    "p","q","r","s","t","u","v","w",
-    "x","y","z","{","|","}","ÅP"
+    "!", "\"", "#", "$", "%", "&", "'",
+    "(", ")", "*", "+", ", ", "-", ".", "/",
+    "0", "1", "2", "3", "4", "5", "6", "7",
+    "8", "9", ":", ";", "<", "=", ">", "?",
+    "@", "A", "B", "C", "D", "E", "F", "G",
+    "H", "I", "J", "K", "L", "M", "N", "O",
+    "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z", "[", "\\", "]", "^", "_",
+    "'", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z", "{", "|", "}", "ÅP"
 };
 static const char SpaceTable[][3] = {
     "Å@",
@@ -208,52 +208,52 @@ static const char SpaceTable[][3] = {
 };
 
 static const BYTE DefaultMacro0[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x4A,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x4A, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro1[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x31,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x31, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro2[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x20,0x41,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x20, 0x41, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro3[] = {
-    0x1B,0x28,0x32,0x1B,0x29,0x34,0x1B,0x2A,0x35,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x32, 0x1B, 0x29, 0x34, 0x1B, 0x2A, 0x35, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro4[] = {
-    0x1B,0x28,0x32,0x1B,0x29,0x33,0x1B,0x2A,0x35,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x32, 0x1B, 0x29, 0x33, 0x1B, 0x2A, 0x35, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro5[] = {
-    0x1B,0x28,0x32,0x1B,0x29,0x20,0x41,0x1B,0x2A,0x35,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x32, 0x1B, 0x29, 0x20, 0x41, 0x1B, 0x2A, 0x35, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro6[] = {
-    0x1B,0x28,0x20,0x41,0x1B,0x29,0x20,0x42,0x1B,0x2A,0x20,0x43,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x20, 0x41, 0x1B, 0x29, 0x20, 0x42, 0x1B, 0x2A, 0x20, 0x43, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro7[] = {
-    0x1B,0x28,0x20,0x44,0x1B,0x29,0x20,0x45,0x1B,0x2A,0x20,0x46,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x20, 0x44, 0x1B, 0x29, 0x20, 0x45, 0x1B, 0x2A, 0x20, 0x46, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro8[] = {
-    0x1B,0x28,0x20,0x47,0x1B,0x29,0x20,0x48,0x1B,0x2A,0x20,0x49,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x20, 0x47, 0x1B, 0x29, 0x20, 0x48, 0x1B, 0x2A, 0x20, 0x49, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacro9[] = {
-    0x1B,0x28,0x20,0x4A,0x1B,0x29,0x20,0x4B,0x1B,0x2A,0x20,0x4C,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x20, 0x4A, 0x1B, 0x29, 0x20, 0x4B, 0x1B, 0x2A, 0x20, 0x4C, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroA[] = {
-    0x1B,0x28,0x20,0x4D,0x1B,0x29,0x20,0x4E,0x1B,0x2A,0x20,0x4F,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x20, 0x4D, 0x1B, 0x29, 0x20, 0x4E, 0x1B, 0x2A, 0x20, 0x4F, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroB[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x20,0x42,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x20, 0x42, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroC[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x20,0x43,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x20, 0x43, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroD[] = {
-    0x1B,0x24,0x39,0x1B,0x29,0x20,0x44,0x1B,0x2A,0x30,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x24, 0x39, 0x1B, 0x29, 0x20, 0x44, 0x1B, 0x2A, 0x30, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroE[] = {
-    0x1B,0x28,0x31,0x1B,0x29,0x30,0x1B,0x2A,0x4A,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x31, 0x1B, 0x29, 0x30, 0x1B, 0x2A, 0x4A, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 static const BYTE DefaultMacroF[] = {
-    0x1B,0x28,0x4A,0x1B,0x29,0x32,0x1B,0x2A,0x20,0x41,0x1B,0x2B,0x20,0x70,0x0F,0x1B,0x7D
+    0x1B, 0x28, 0x4A, 0x1B, 0x29, 0x32, 0x1B, 0x2A, 0x20, 0x41, 0x1B, 0x2B, 0x20, 0x70, 0x0F, 0x1B, 0x7D
 };
 
 CARIB8CharDecode::CARIB8CharDecode(void)
@@ -2042,7 +2042,7 @@ BOOL CARIB8CharDecode::DRCSHeaderparse(const BYTE *pbSrc, DWORD dwSrcSize, BOOL 
                         drcs.bmiHeader = bmiHeader;
 
                         //Ç«ÇÒÇ»îzêFÇ…ÇµÇƒÇ‡ç\ÇÌÇ»Ç¢ÅBcolors[4]à»è„ÇÃêFÇÕèoåªÇµÇ»Ç¢
-                        RGBQUAD colors[16] = {{255,255,255,0}, {170,170,170,0}, {85,85,85,0}, {0,0,0,0}};
+                        RGBQUAD colors[16] = { {255, 255, 255, 0}, { 170, 170, 170, 0 }, { 85, 85, 85, 0 }, { 0, 0, 0, 0 } };
                         BITMAPFILEHEADER bmfHeader = { 0 };
                         bmfHeader.bfType = 0x4D42;
                         bmfHeader.bfOffBits = sizeof(bmfHeader) + sizeof(bmiHeader) + sizeof(colors);

src/Caption2Ass_PCR/Caption2AssParameter.h
@@ -70,9 +70,9 @@ public:
 
     int Allocate(size_t string_length = 0);
 
-    pid_information_t * get_pid_information(void){ return &pid_information; }
-    cli_parameter_t * get_cli_parameter(void){ return &cli_parameter; }
-    ass_setting_t * get_ass_setting(void){ return &ass_setting; }
+    pid_information_t *get_pid_information(void){ return &pid_information; }
+    cli_parameter_t *get_cli_parameter(void){ return &cli_parameter; }
+    ass_setting_t *get_ass_setting(void){ return &ass_setting; }
 };
 
 #endif // __CAPTION2ASS_PARAMETER_H__

commit c38fa4bc076fb75a728f63c6ac58c0082a316553

all: Fix a bug caused by editing mistakes
Regression in 2f24c42bafc75848a3d20a52c21ca7632e466fa4 .

src/Caption_dll/ARIB8CharDecode.cpp
@@ -189,7 +189,7 @@ static const char KanaTable[][3] = {
 // 半角用のテーブル
 static const char HalfAsciiTable[][3] = {
     "!", "\"", "#", "$", "%", "&", "'",
-    "(", ")", "*", "+", ", ", "-", ".", "/",
+    "(", ")", "*", "+", ",", "-", ".", "/",
     "0", "1", "2", "3", "4", "5", "6", "7",
     "8", "9", ":", ";", "<", "=", ">", "?",
     "@", "A", "B", "C", "D", "E", "F", "G",

src/common/CommRoutine.cpp
@@ -54,7 +54,7 @@ static const char HalfHiraTable[][3] = {
     "ÇÓ", "ÇÔ", "Ç", "ÇÒ", " ", " ", " ", "ÅT",
     "ÅU", "∞", "°", "¢", "£", "§", "•",
     "!", "\"", "#", "$", "%", "&", "'",
-    "(", ")", "*", "+", ", ", "-", ".", "/",
+    "(", ")", "*", "+", ",", "-", ".", "/",
     "0", "1", "2", "3", "4", "5", "6", "7",
     "8", "9", ":", ";", "<", "=", ">", "?",
     "@", "A", "B", "C", "D", "E", "F", "G",